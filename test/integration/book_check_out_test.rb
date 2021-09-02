7bcf314c74da9e9c144bd2b656e2003c7d33e614
Nelson Perez <nelsonperflo@gmail.com>
2021-09-02 10:55:58 -0600

    add last validations

--- test/integration/book_check_out_test.rb
+++ test/integration/book_check_out_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class BookCheckOutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:james)
+    @book = books(:the_da_vinci_code)
+  end
+
+  test "check out book" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'Fargo01$-' } }
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_equal "Hello <strong>James</strong>!!!", flash[:success]
+    get book_path(@book)
+    assert_response :success
+    assert_equal @book.stock.quantity, 1
+    patch checkout_book_path(@book)
+    assert_redirected_to book_path(@book)
+  end
 end
